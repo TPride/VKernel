0f6f302141b38a389236f557d5226759a942035c
骄傲 <61260563+TPride@users.noreply.github.com>
2020-02-23 04:50:21 +0800

    Update

---
+++ src/vkernel/interfaces/event/GameEvent.java
@@ -0,0 +1,25 @@
+package vkernel.interfaces.event;
+
+import cn.nukkit.event.Event;
+import cn.nukkit.event.HandlerList;
+import vkernel.interfaces.Room;
+
+/**
+ * Created by TPride on 2020/2/23.
+ */
+public class GameEvent extends Event {
+    protected Room room;
+    private static final HandlerList handlerList = new HandlerList();
+
+    public GameEvent(Room room) {
+        this.room = room;
+    }
+
+    public final Room getRoom() {
+        return room;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+}
